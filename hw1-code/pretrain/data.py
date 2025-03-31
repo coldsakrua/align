a115b8bc6506170be2cd1b93380a681b420cb663
liyang <guzhoudiaoxue0706@163.com>
2025-03-31 20:06:56 +0800

    2100013161 dataloader

--- hw1-code/pretrain/data.py
+++ hw1-code/pretrain/data.py
@@ -1,22 +1,40 @@
 import torch
 from torch.utils.data import Dataset,DataLoader
+from transformers import GPT2Tokenizer
+
 
 class DataloaderLite:
-    def __init__(self,batch,seq_len,process_rank,num_processes,split):
+    def __init__(self,data_root,batch,seq_len,process_rank,num_processes,split,tokenizer):
+        self.data_root=data_root
         self.batch=batch
         self.seq_len=seq_len
         self.process_rank=process_rank
         self.num_processes=num_processes
+        self.current_batch=0
         assert split in ['train','val']
         self.split=split
         
-        
+        with open(os.path.join(self.data_root)) as f:
+            self.data=f.read()
 
+        self.tokenizer=tokenizer
+        self.tokens=torch.tensor(self.tokenizer.encode(self.data),dtype=torch.long)
 
-    def __len__(self):
-        return len(self.data)
+        print(f"total tokens:{len(self.tokens)}")
+        print(f"batches:{len(self.tokens)//(self.batch*self.seq_len)}")
+
+    def next_batch(self):
+        start_idx=self.current_batch*self.batch*self.seq_len
+        end_idx=(self.current_batch+1)*self.batch*self.seq_len
+        buf=self.tokens[start_idx:end_idx]
+        x=buf[:self.seq_len].view(self.batch,self.seq_len)
+        y=buf[1:self.seq_len+1].view(self.batch,self.seq_len)
+        if buf.size(0)<self.seq_len+1:
+            x=torch.cat([x,torch.zeros(self.batch,self.seq_len-buf.size(0)+1,dtype=torch.long)],dim=1)
+            y=torch.cat([y,torch.zeros(self.batch,self.seq_len-buf.size(0)+1,dtype=torch.long)],dim=1)
+        self.current_batch+=1
+        return x,y
     
-    def __getitem__(self,idx):
-        return self.data[idx]
-        
+    def reset_batch_pointer(self):
+        self.current_batch=0
         
